5568e1caff0ee4c2d77911bfa4ab99f9cf6b664c
LeslieLYM <worldsendlover@gmail.com>
2021-04-20 14:26:33 +0800

    Setup game logic elements - Created slots prefab - Created player statistics - Created scripts for process the main logic

---
+++ PlayStudiosTechChallenge/Assets/ScriptableObjects/Player/SO_PlayerStat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Game Statistic/Player Statistic", fileName = "PlayerStatistic")]
+public class SO_PlayerStat : ScriptableObject
+{
+    public int totalPoints;
+    public int totalPicks;
+
+    public void StoreNewPoints(int pt)
+    {
+        totalPoints += pt;
+    }
+
+    public void UsePicks(int i)
+    {
+        if (totalPicks - i < 0)
+        {
+            Debug.LogError("Total pick will drop below zero. Current pick use is skipped but check the pick processing method.");
+            return;
+        }
+        totalPicks -= i;
+    }
+}
---
+++ PlayStudiosTechChallenge/Assets/Scripts/Slot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slot : MonoBehaviour
+{
+    public void RevealPrize()
+    {
+        print("you got prize!");
+    }
+}
